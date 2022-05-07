a66d99e034e8ed3c3d25136d8ab1cf034fcfb564
morshedulmunna <morshedulmunna1@gmail.com>
2022-05-08 00:31:01 +0600

    clan code

--- index.js
+++ index.js
@@ -4,23 +4,26 @@ const jwt = require("jsonwebtoken");
 const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 
-// Require =>
+// Require ==========>
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 // Middle-Ware
 app.use(cors());
 app.use(express.json());
+
 // DB Connection
 const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hkk7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+// Create DataBase Client======>>>
 const client = new MongoClient(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// Database Run funtion with try finnaly =====>>>>
 const run = async () => {
   try {
+    // Connect DataBase
     await client.connect();
     console.log("DB Connected");
     const productsCollection = client.db("hikingRoX").collection("products");
@@ -118,7 +121,7 @@ app.listen(PORT, () => {
   console.log("server is running port", PORT);
 });
 
-// verify token function
+// verify token function =======>>>
 function verifyToken(token) {
   let email;
   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
